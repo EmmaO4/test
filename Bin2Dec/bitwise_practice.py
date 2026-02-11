f214ce821a22dedfda514298f274ab33344baa79
raul <raulesquerra18@gmail.com>
2026-02-11 08:02:54 -0800

    restructured files

--- Bin2Dec/bitwise_practice.py
+++ Bin2Dec/bitwise_practice.py
@@ -1,14 +1,11 @@
 """
 bitwise practice to implement bitwise functions for better algo runtime on bin2dec.py
 """
-print(5 & 6)
-print(bin(5))
-print(bin(6))
-to_print = "hello"
-print(to_print.strip("hell"))
+print(f'five: \t{bin(5)}')
+print(f'six: \t{bin(6)}')
 
 five = bin(5)
 six = bin(6)
-print(five.strip("0b"))
-print(six.strip("0b"))
-print(5 & 6)
+print(f'five: \t{five.lstrip("0b")}')
+print(f'six: \t{six.lstrip("0b")}')
+print(f'5 & 6: \t{5 & 6}')  
---
+++ sandbox/read_list_reverse.py
@@ -0,0 +1,10 @@
+list = [3, 4, 12, 13]       #, 44, 25, 69, 75, 28, 9]
+ret = []
+
+
+
+for i in list[::-1]:
+    ret.append(i)
+
+print(ret)
+    
